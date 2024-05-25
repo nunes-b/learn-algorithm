3509da8e656b6340c153414e6f2ab73d3e0dfde9
Romulo Nunes <contatornb.peixoto@gmail.com>
2024-05-25 15:19:54 -0300

    [feat ] order_arr

--- min_indice.py
+++ min_indice.py
@@ -1,16 +1,26 @@
-def search_min_indice(list):
-    min = list[0]
-    min_indice = min
+def search_min_indice(lst):
+    min_val = lst[0]
+    min_indice = 0
 
-    for indice in range(1, len(list)):
-        if list[indice] < min:
-            min = list[indice]
+    for indice in range(1, len(lst)):
+        if lst[indice] < min_val:
+            min_val = lst[indice]
             min_indice = indice
 
     return min_indice
 
 
-list = [1, 3, 5, 7, 9]
-search_min_indice = search_min_indice(list)
+def order_arr(arr):
+    sorted_arr = []
+    arr_copy = arr[:]
 
-print(search_min_indice)
+    for _ in range(len(arr)):
+        min_index = search_min_indice(arr_copy)
+        sorted_arr.append(arr_copy.pop(min_index))
+
+    return sorted_arr
+
+
+arr1 = [11, 1, 90, 3, 5, 7, 9]
+sorted_arr = order_arr(arr1)
+print(sorted_arr)
