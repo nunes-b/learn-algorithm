ae866ac5c0a72d17a356ee9638c2159537881276
Rômulo Nunes <romulotg21@gmail.com>
2024-05-27 21:00:49 -0300

    [feat] recursive function

---
+++ recursive_function.py
@@ -0,0 +1,6 @@
+def procure_pela_chave(caixa):
+    for item in caixa:
+        if item.e_uma_caixa():
+            procure_pela_chave(item)
+        elif item.e_uma_chave():
+            return "achei a chaveee"
