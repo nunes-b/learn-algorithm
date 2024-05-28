774b47f976970bcadefa6a099ba1820c404d9067
Rômulo Nunes <romulotg21@gmail.com>
2024-05-27 21:33:11 -0300

    [learn] pilha e recursao

---
+++ ex_recursao.py
@@ -0,0 +1,16 @@
+caixa_sala = [["chave"], [], []]
+caixas_cozinha = [[], [], []]
+caixas_quarto = [[], [], []]
+
+caixas = [caixa_sala, caixas_cozinha, caixas_quarto]
+
+
+def procure_pela_chave(caixas):
+    for indice, pilha in enumerate(caixas):
+        for obj, caixa in enumerate(pilha):
+            if "chave" in caixa:
+                return f"A chave está na pilha {indice}, caixa {obj}"
+    return "A chave não foi encontrada"
+
+
+print(procure_pela_chave(caixas))
